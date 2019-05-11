016c350b6b6239681827b2f7c542d8c25b87ca7e
Daniel Rohr <rohrdr@gmail.com>
2019-05-11 16:34:20 +0200

    more PEP8 conform

--- temp.py
+++ temp.py
@@ -29,7 +29,6 @@ def load_data():
     test_x_orig = np.array(test_dataset["test_set_x"][:])
     test_set_y_orig = np.array(test_dataset["test_set_y"][:])
 
-
     # classes = np.array(test_dataset["list_classes"][:]) # the list of classes
 
     # reshaping dimensions
@@ -48,21 +47,21 @@ def load_data():
     num_px = train_x_orig.shape[1]
     m_test = test_x_orig.shape[0]
 
-    print ("\nSizes of sets")
-    print ("Number of training examples: " + str(m_train))
-    print ("Number of testing examples: " + str(m_test))
-    print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-    print ("\nShapes of training set")
-    print ("train_x_orig shape: " + str(train_x_orig.shape))
-    print ("train_x's shape: " + str(train_x.shape))
-    print ("train_set_y_orig shape: " + str(train_set_y_orig.shape))
-    print ("train_y shape: " + str(train_y.shape))
-    print ("\nShapes of test set")
-    print ("test_x_orig shape: " + str(test_x_orig.shape))
-    print ("test_x's shape: " + str(test_x.shape))
-    print ("test_set_y_orig shape: " + str(test_set_y_orig.shape))
-    print ("test_y shape: " + str(test_y.shape))
-    print ("\n\n\n")
+    print("\nSizes of sets")
+    print("Number of training examples: " + str(m_train))
+    print("Number of testing examples: " + str(m_test))
+    print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+    print("\nShapes of training set")
+    print("train_x_orig shape: " + str(train_x_orig.shape))
+    print("train_x's shape: " + str(train_x.shape))
+    print("train_set_y_orig shape: " + str(train_set_y_orig.shape))
+    print("train_y shape: " + str(train_y.shape))
+    print("\nShapes of test set")
+    print("test_x_orig shape: " + str(test_x_orig.shape))
+    print("test_x's shape: " + str(test_x.shape))
+    print("test_set_y_orig shape: " + str(test_set_y_orig.shape))
+    print("test_y shape: " + str(test_y.shape))
+    print("\n\n\n")
 
     return train_x, train_y, test_x, test_y
 
@@ -82,31 +81,31 @@ np.random.seed(1)
 
 
 # create input for the neural net identical that of Andrew Ng
-layers = []
+layers = list()
 layers.append([train_x.shape[0], 7, af.ReLU()])
 layers.append([7, 1, af.Sigmoid()])
 
 # initiate denseDN
-myDN = dn.DenseNN(layers, cf.CrossEntropy(), learning_rate = 0.0075)
+my_dn = dn.DenseNN(layers, cf.CrossEntropy(), learning_rate=0.0075)
 
 # train the neural net
-myDN.train_dn(train_x, train_y, maxiter = 2500, print_frequency = 100)
+my_dn.train_dn(train_x, train_y, maxiter=2500, print_frequency=100)
 
-Yhat = myDN.forward_propagation(train_x)
-cost = myDN.lossFunc.get_loss(Yhat, train_y)
+yhat = my_dn.forward_propagation(train_x)
+cost = my_dn.lossFunc.get_loss(yhat, train_y)
 
-Yhatclass = np.where(Yhat > 0.5, 1, 0)
+yhatclass = np.where(yhat > 0.5, 1, 0)
 
-print ("The cost after training the network is " + str(cost))
-print ("Accuracy = " + str(np.sum((Yhatclass == train_y) / m_train)))
+print("The cost after training the network is " + str(cost))
+print("Accuracy = " + str(np.sum((yhatclass == train_y) / m_train)))
 
 # the test set
-Yhat_test = myDN.forward_propagation(test_x)
-cost_test = myDN.lossFunc.get_loss(Yhat_test, test_y)
-Yhatclass_test = np.where(Yhat_test > 0.5, 1, 0)
-print ("The cost of the test set is " + str(cost_test))
-print ("Accuracy = " + str(np.sum((Yhatclass_test == test_y) / m_test)))
-print ("\n\n\n")
+yhat_test = my_dn.forward_propagation(test_x)
+cost_test = my_dn.lossFunc.get_loss(yhat_test, test_y)
+yhatclass_test = np.where(yhat_test > 0.5, 1, 0)
+print("The cost of the test set is " + str(cost_test))
+print("Accuracy = " + str(np.sum((yhatclass_test == test_y) / m_test)))
+print("\n\n\n")
 
 
 #
@@ -117,27 +116,26 @@ print ("\n\n\n")
 # when comparing the results, make sure that the random
 # numbers in Layers line 39 are scaled by / np.sqrt(nx)
 np.random.seed(1)
-layers4 = []
+layers4 = list()
 layers4.append([train_x.shape[0], 20, af.ReLU()])
 layers4.append([20, 7, af.ReLU()])
 layers4.append([7, 5, af.ReLU()])
 layers4.append([5, 1, af.Sigmoid()])
-myDN4 = dn.denseDN(layers4, cf.CrossEntropy(), learning_rate = 0.0075)
-myDN4.train_dn(train_x, train_y, maxiter = 2500, print_frequency = 100)
+my_dn4 = dn.DenseNN(layers4, cf.CrossEntropy(), learning_rate=0.0075)
+my_dn4.train_dn(train_x, train_y, maxiter=2500, print_frequency=100)
 
-Yhat = myDN4.forward_propagation(train_x)
-cost = myDN4.lossFunc.get_loss(Yhat, train_y)
+yhat = my_dn4.forward_propagation(train_x)
+cost = my_dn4.lossFunc.get_loss(yhat, train_y)
 
-Yhatclass = np.where(Yhat > 0.5, 1, 0)
+yhatclass = np.where(yhat > 0.5, 1, 0)
 
-print ("The cost after training the network is " + str(cost))
-print ("Accuracy = " + str(np.sum((Yhatclass == train_y) / m_train)))
+print("The cost after training the network is " + str(cost))
+print("Accuracy = " + str(np.sum((yhatclass == train_y) / m_train)))
 
 # the test set
-Yhat_test = myDN4.forward_propagation(test_x)
-cost_test = myDN4.lossFunc.get_loss(Yhat_test, test_y)
-Yhatclass_test = np.where(Yhat_test > 0.5, 1, 0)
-print ("The cost of the test set is " + str(cost_test))
-print ("Accuracy = " + str(np.sum((Yhatclass_test == test_y) / m_test)))
-print ("\n\n\n")
-
+yhat_test = my_dn4.forward_propagation(test_x)
+cost_test = my_dn4.lossFunc.get_loss(yhat_test, test_y)
+yhatclass_test = np.where(yhat_test > 0.5, 1, 0)
+print("The cost of the test set is " + str(cost_test))
+print("Accuracy = " + str(np.sum((yhatclass_test == test_y) / m_test)))
+print("\n\n\n")
